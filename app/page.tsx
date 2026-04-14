411d3ad093637ccb1420d71f6911464fa62bd336
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-04-14 20:26:39 +0900

    [add] 基本機能を実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,99 @@
-import Image from "next/image";
+"use client"
+import { useState, useRef } from "react";
+
+type Stamp = {
+  id: number;
+  text: string;
+  left: number;
+}
+
+type Word = {
+  id: number;
+  word: string;
+}
 
 export default function Home() {
+
+  const [stamps, setStamps] = useState<Stamp[]>([]);
+  const [disable, setDisable] = useState(false);
+  const idRef = useRef(0);
+
+  const words:Word[] = [
+    {
+      id: 1,
+      word: "疲れた"
+    },
+    {
+      id:2,
+      word:"お腹すいた",
+    },
+    {
+      id:3,
+      word:"よくやった"
+    }
+  ]
+
+  const tired = (type: number) => {
+
+    if(disable) return;
+
+    setDisable(true);
+
+    const selected = words.find(item => item.id === type);
+    if(!selected) return;
+
+    const newStampId = ++idRef.current;
+
+    // スタンプ追加
+    setStamps((prev) => [
+      ...prev,
+      {
+        id: newStampId,
+        text: selected.word,
+        left: Math.random() * 80
+      }
+    ]);
+
+    // 一定時間ごに削除
+    setTimeout(() => {
+      setStamps((prev) => prev.filter(s => s.id !== newStampId));
+    }, 20000);
+
+    setTimeout(() => {
+      setDisable(false);
+    }, 2000);
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <>
+      <div className="relative h-[200px] border m-10">
+        {stamps.map((stamp) => (
+          <p
+            key={stamp.id}
+            className="absolute animate-float text-indigo-500 font-bold"
+            style={{
+              left: `${stamp.left}%`,
+            }}
+          >
+            {stamp.text}
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        ))}
+      </div>
+
+
+      <div className="flex gap-10 justify-center">
+        {words.map((item) => (
+          <div key={item.id} className="w-fit">
+            <button
+              disabled={disable}
+              onClick={()=>tired(item.id)}
+              className="disabled:text-gray-500 bg-indigo-500 text-white px-3"
+            >
+              {item.word}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
