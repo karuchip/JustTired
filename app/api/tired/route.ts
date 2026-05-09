a2141afd1c2cd004af7c4fa8190fee100c0c6755
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-05-09 18:28:09 +0900

    [add] 疲れたカウントの表示機能追加

--- app/api/tired/route.ts
+++ app/api/tired/route.ts
@@ -1,48 +1,71 @@
 import { sql } from "@/src/neon/neon";
 
 export async function GET(req: Request) {
-  const {searchParams} = new URL(req.url);
 
-  // 初期化用（現在の最大IDだけ返す）
-  const mode = searchParams.get('mode')
-  if (mode === 'init') {
+  try {
+    const {searchParams} = new URL(req.url);
+
+    // 初期化用
+    const mode = searchParams.get('mode')
+    if (mode === 'init') {
+
+      // 現在の最大投稿IDを返す
+      const result = await sql `
+        SELECT MAX(id) as max_id FROM tired_posts
+      `;
+      return Response.json(result[0]?.max_id ?? 0)
+    }
+
+    // 匿名ID取得
+    const anonymousId = searchParams.get('anonymousId');
+    if(!anonymousId) {
+      return Response.json({error: "anonymousId is required"}, {status: 400})
+    }
+
+    // 差分取得処理
+    const raw = searchParams.get('lastId')
+    const lastId = Number(raw);
+    const safeId = isNaN(lastId) ? 0 : lastId;
+
     const result = await sql `
-      SELECT MAX(id) as max_id FROM tired_posts`
-    ;
-    return Response.json(result[0]);
+      SELECT * FROM tired_posts
+      WHERE id > ${safeId}
+      AND anonymous_id <> ${anonymousId}
+      ORDER BY created_at ASC
+    `;
+    return Response.json(result);
+
+  }catch(error) {
+    console.error(error);
+    return Response.json(
+      {error: 'server error'},
+      {status: 500}
+    )
   }
-
-  // 匿名ID取得
-  const anonymousId = searchParams.get('anonymousId');
-  if(!anonymousId) {
-    return Response.json({error: "anonymousId is required"}, {status: 400})
-  }
-
-  // 差分取得処理
-  const raw = searchParams.get('lastId')
-  const lastId = Number(raw);
-  const safeId = isNaN(lastId) ? 0 : lastId;
-
-  const result = await sql `
-    SELECT * FROM tired_posts
-    WHERE id > ${safeId}
-    AND anonymous_id <> ${anonymousId}
-    ORDER BY created_at ASC
-  `;
-  return Response.json(result);
 }
 
+
+// ボタン押下時
 export async function POST(req:Request) {
-  const {text, anonymousId} = await req.json();
 
-  if(typeof text !== "string") {
-    return Response.json({error: "invalid"}, {status: 400});
+  try {
+    const {text, anonymousId} = await req.json();
+
+    if(typeof text !== "string" || typeof anonymousId !== "string") {
+      return Response.json({error: "invalid"}, {status: 400});
+    }
+
+    // 投稿保存
+    await sql`
+    INSERT INTO tired_posts (text, anonymous_id)
+    VALUES (${text}, ${anonymousId})
+    `;
+
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      {error: 'server error'},
+      {status: 500}
+    )
   }
-
-  await sql`
-  INSERT INTO tired_posts (text, anonymous_id)
-  VALUES (${text}, ${anonymousId})
-  `;
-
-  return Response.json({ok: true});
 }
--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,15 @@ type Word = {
   word: string;
 }
 
-type TiredItem = {
+type TiredItemType = {
   id: number;
   text: string;
   created_at: Date;
+}
+
+type CountItemType = {
+  daily_count: number;
+  total_count: number;
 }
 
 export default function Home() {
@@ -31,6 +36,10 @@ export default function Home() {
 
   // 一度表示したスタンプのidは保存
   const lastIdRef = useRef(0);
+
+  // 疲れた件数を保存
+  const [dailyCount, setDailyCount] = useState<number>(0);
+  const [totalCount, setTotalCount] = useState<number>(0);
 
   // 初期化終わるまでfetchしないためのフラグ
   const [initialized, setInitialized] = useState(false);
@@ -64,26 +73,33 @@ export default function Home() {
       // ①匿名ID準備
       anonymousIdRef.current = getAnonymousId();
 
-      // ②現在の最大投稿ID取得
-      const res = await fetch('/api/tired?mode=init');
-      const data = await res.json();
+      // ②DBから現在の最大投稿IDを取得
+      const resMaxId = await fetch('/api/tired?mode=init');
+      const dataMaxId = await resMaxId.json();
 
-      lastIdRef.current = data.max_id ?? 0;
+      lastIdRef.current = dataMaxId.max_id;
 
-      // ③初期化完了
+      // ③DBから疲れたカウント数を取得
+      const resCount = await fetch('/api/tired/stats')
+      const dataCount = await resCount.json();
+      setDailyCount(dataCount.daily_count);
+      setTotalCount(dataCount.total_count);
+
+      // ④初期化完了
       setInitialized(true);
     };
 
     init();
   }, []);
 
-  // 差分だけ取得
+
+  // 投稿の差分取得 ポーリングにて3秒間隔で
   useEffect(() => {
     if(!initialized) return;
 
     const fetchData = async () => {
       const res = await fetch(`/api/tired?lastId=${lastIdRef.current}&anonymousId=${anonymousIdRef.current}`);
-      const data: TiredItem[] = await res.json();
+      const data: TiredItemType[] = await res.json();
 
       setList(prev => {
 
@@ -91,7 +107,7 @@ export default function Home() {
 
         const newItems = data
         .filter(item => !existingIds.has(item.id))
-        .map((item: TiredItem) => ({
+        .map((item: TiredItemType) => ({
           id: item.id,
           text: item.text,
           left: Math.random() * 80
@@ -113,7 +129,27 @@ export default function Home() {
   }, [initialized])
 
 
-  // 疲れたボタン押下時
+  // 疲れた件数の取得　ポーリングで10秒ごとに
+  useEffect(() => {
+    if(!initialized) return;
+
+    const fetchCount = async() => {
+      const res = await fetch(`api/tired/stats`);
+      const data:CountItemType = await res.json();
+
+      setDailyCount(data.daily_count);
+      setTotalCount(data.total_count);
+    }
+
+    fetchCount();
+    const countInterval = setInterval(fetchCount, 10000);
+
+    return ()=> clearInterval(countInterval)
+
+  },[initialized])
+
+
+  // 疲れたボタン押下時の処理
   const handleSendStamp = async(type: number) => {
 
     if(disable) return;
@@ -123,9 +159,12 @@ export default function Home() {
     const selected = words.find(item => item.id === type);
     if(!selected) return;
 
-    const newStampId = ++idRef.current;
+    // ⓪画面上の疲れたカウントをプラス1
+    setTotalCount(prev => prev + 1);
+    setDailyCount(prev => prev + 1);
 
-    // ①まず画面に表示
+    // ①画面上に疲れたを表示
+    const newStampId = ++idRef.current;
     setStamps((prev) => [
       ...prev,
       {
@@ -138,10 +177,10 @@ export default function Home() {
     // ②一定時間後に画面から削除
     setTimeout(() => {
       setStamps((prev) => prev.filter(s => s.id !== newStampId));
-    }, 20000);
+    }, 5000);
 
 
-    // ③バックグラウンドでDBに保存
+    // ③バックグラウンドで投稿をDBに保存
     await fetch('/api/tired', {
       method: 'POST',
       headers: {
@@ -153,6 +192,11 @@ export default function Home() {
       })
     });
 
+    // ④バックグラウンドでDBの疲れたカウントを+1
+    await fetch('/api/tired/stats', {
+      method: 'POST'
+    })
+
     setTimeout(() => {
       setDisable(false);
     }, 1000);
@@ -162,6 +206,11 @@ export default function Home() {
     <div>
 
       <div className="relative z-100">
+
+        <div>
+          <p>これまでの「疲れた」{totalCount}回</p>
+          <p>今日の「疲れた」{dailyCount}回</p>
+        </div>
         <div className="mt-10">
           <h1 className="text-[#ffffff] text-[32px] md:text-[68px] w-fit mx-auto">Just Tired</h1>
           <h2 className="text-[#ffffff] w-fit mx-auto text-[16px] md:text-[22px]">疲れたって言いたい！</h2>
@@ -175,7 +224,9 @@ export default function Home() {
 
                 <p
                 className="
-                  absolute animate-float font-bold text-[26px] md:text-[42px] whitespace-nowrap
+                  absolute animate-float-mine font-bold text-[26px] md:text-[42px] whitespace-nowrap
+                  px-6 py-1 bg-white/50 rounded-full
+
                   /* 文字の色：少し薄い色にすると光って見えます */
                   text-[#ffffff]
                   /* ネオンエフェクト（外側の光） */
@@ -221,7 +272,7 @@ export default function Home() {
             {list.map((li) => (
               <p
               key={`other-${li.id}`}
-              className="absolute animate-float text-[#ffffff] font-bold"
+              className="absolute animate-float-other text-gray font-bold"
               style={{
                 left: `${li.left}%`,
               }}
